b03c1ef9af05ec0667ddb4a65fbfca833985d27f
poudelpawan770-lab <poudelpawan770@gmail.com>
2025-11-27 12:19:15 +0545

    initial commit

---
+++ EvenPrint.java
@@ -0,0 +1,13 @@
+class EvenPrint{
+    public static void main(String []args){
+        int i = 2;
+        //printing the  even numbers from 2 to 20 
+        while(i <= 20){
+            System.out.println(i);
+            i += 2;
+            
+        }
+         
+        
+    }
+}
