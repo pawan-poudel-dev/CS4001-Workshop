1e4795d3713518d491a34d09f4bf57e40f93eb1c
pawan_poudel_Devloper <poudelpawan770@gmail.com>
2026-01-22 12:13:38 +0545

    Add files via upload

---
+++ ArrayPratice/VShapedTriangle.java
@@ -0,0 +1,16 @@
+public class VShapedTriangle {
+    public static void main(String [] args){
+        int n = 5; //V shaped triangle of  height 5
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < 2*n - 1; j++){
+                if(j == i || j == (2*n - 2 - i)){
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
